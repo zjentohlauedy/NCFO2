c5121d0dcf8c91f15d6cc46dffb7bd4390c30d92
Ed Hewell <ghewelljr@gmail.com>
2014-09-21 21:22:02 -0700

    Standings ruby wrapper now works without save states

--- Ruby/standings.rb
+++ Ruby/standings.rb
@@ -9,6 +9,10 @@ path = ARGV[0] || '.'
 
 standings = ProgRunner.new "#{location}/../C", "standings"
 
-standings.execute "#{path}/ncfo1.nes", "#{path}/ncfo1.nst", "#{path}/ncfo2.nes", "#{path}/ncfo2.nst"
+if File.file?("#{path}/ncfo1.nst") and File.file?("#{path}/ncfo2.nst")
+  standings.execute "#{path}/ncfo1.nes", "#{path}/ncfo1.nst", "#{path}/ncfo2.nes", "#{path}/ncfo2.nst"
+else
+  standings.execute "#{path}/ncfo1.nes", "#{path}/ncfo2.nes"
+end
 
 puts standings.get_output
