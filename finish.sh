ca59e1cc65b4e7a83bbecfcbfb7ff3ecee816d15
Ed Hewell <ghewelljr@gmail.com>
2016-09-25 19:25:22 -0700

    Updating libre office command line for csv conversion

--- finish.sh
+++ finish.sh
@@ -8,6 +8,6 @@ then
 fi
 
 echo "Converting schedule.ods to csv..."
-soffice -env:UserInstallation=file://~/.libreoffice-alt --headless --convert-to csv --outdir . ./schedule.ods
+soffice -env:UserInstallation=file://$HOME/.libreoffice-alt --headless --convert-to csv --outdir . ./schedule.ods
 
 ~/NES/NCFO/Ruby/boxscores.rb
